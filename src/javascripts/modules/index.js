68cf847b3e1ae50268931e13a66d8ce0b0c500e8
praveenjuge <praveenjuge@gmail.com>
2017-07-05 17:03:34 +0530

    More UI Updates

--- src/javascripts/modules/index.js
+++ src/javascripts/modules/index.js
@@ -8,27 +8,76 @@ var headroom  = new Headroom(myElement, {
 // initialise
 headroom.init();
 
-
 // ****************************************************************************
 
-
-var elem = document.querySelector('.grid');
-var iso = new Isotope( elem, {
+var $grid = $('.grid').isotope({
   // options
   itemSelector: '.grid-item',
   layoutMode: 'vertical'
 });
 
-// element argument can be a selector string
-//   for an individual element
-var iso = new Isotope( '.grid', {
-  layoutMode: 'vertical'
+$('.osbuttons').on( 'click', 'li', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
 
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  iso.isotope({ filter: filterValue });
+// ****************************************************************************
+
+
+$('.nav-toggle').click(function(e){
+		e.stopPropagation();
+		$(this).toggleClass('open');
+		$('.ham-container').toggleClass('nav-open');
 });
+$('.ham-container').click(function(e){
+		e.stopPropagation();
+});
+$('body,html,li').click(function(e){
+	 $('.nav-toggle').removeClass('open');
+	 $('.ham-container').removeClass('nav-open');
+});
+
+
+// ****************************************************************************
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 
 // ****************************************************************************
 
